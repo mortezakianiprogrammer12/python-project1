d3ccf4d8fb9e395fb2a04126ae20e846ef9960c7
Morteza Kisni <kisnimorteza30@gmail.com>
2025-10-29 18:01:58 +0000

    Initial commit

---
+++ first-exercise.py
@@ -0,0 +1,13 @@
+# گرفتن سن از کاربر
+age = int(input("لطفا سن  خود را به سال وارد کنید: "))
+
+# محاسبه بر حسب سال
+days = age * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+# نمایش خروجی
+print("سن شما به ساعت:", hours)
+print("سن شما به دقیقه:", minutes)
+print("سن شما به ثانیه:", seconds)
